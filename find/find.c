043b5df5d4d3018e8ea46b55de2b641d39b7518c
Jhonfer Ruiz <jhonfer.ruiz@flyr.com>
2026-01-06 15:12:16 -0500

    modified code for adapt lecture

--- find/find.c
+++ find/find.c
@@ -3,19 +3,18 @@
 int main(int argc, char *argv[]) {
   char verified = verifiedFlags(argc, argv);
   if (argc != 3 || verified) {
-    if (argc == 2 && verified) {
-      helpCommand();
-      return EXIT_FAILURE;
-    }
-    status = 1;
+    status = argc == 2 && verified ? status : 1;
     helpCommand();
     return EXIT_FAILURE;
   }
+
   findFile(argv[1], argv[2]);
+
   if (status == 0) {
     printf("%sEl patrón de búsqueda no se ha encontrado%s\n", PURPLECOLOUR,
            ENDCOLOUR);
   }
+
   printf("\n%s Se han encontrado %s%d%s %sarchivos y %s%d%s %scarpetas en la "
          "búsqueda %s\n",
          PURPLECOLOUR, GREENCOLOUR, amountFiles, ENDCOLOUR, PURPLECOLOUR,
@@ -32,57 +31,63 @@ char verifiedFlags(int size, char *args[]) {
 }
 
 void helpCommand() {
-  if (status == 0) {
-    printf("%s./find%s  %s/path/to/file/ nameFileFind%s\n", PURPLECOLOUR,
-           ENDCOLOUR, YELLOWCOLOUR, ENDCOLOUR);
-    printf("\t%s/path/to/file/%s %sRuta o directorio donde desea empezar la "
-           "búsqueda%s\n",
-           YELLOWCOLOUR, ENDCOLOUR, BLUECOLOUR, ENDCOLOUR);
-    printf("\t%snameFileFind%s   %sNombre del archivo a buscar%s\n",
-           YELLOWCOLOUR, ENDCOLOUR, BLUECOLOUR, ENDCOLOUR);
-  } else if (status == 1) {
+
+  if (status == 1) {
     printf(
         "%sEspecifique un patrón de búsqueda válido. Ver ayuda -h o --help%s\n",
         REDCOLOUR, ENDCOLOUR);
+    return;
   }
+
+  printf("%s./find%s  %s/path/to/file/ nameFileFind%s\n", PURPLECOLOUR,
+           ENDCOLOUR, YELLOWCOLOUR, ENDCOLOUR);
+  printf("\t%s/path/to/file/%s %sRuta o directorio donde desea empezar la "
+           "búsqueda%s\n",
+           YELLOWCOLOUR, ENDCOLOUR, BLUECOLOUR, ENDCOLOUR);
+  printf("\t%snameFileFind%s   %sNombre del archivo a buscar%s\n",
+           YELLOWCOLOUR, ENDCOLOUR, BLUECOLOUR, ENDCOLOUR);
 }
 
 void findFile(char *path, char *pattern) {
   DIR *directory;
   struct dirent *content;
   struct stat fileStat;
-  char *path2, *name2;
+  char *pathBuilder, *nameFileContent;
 
   directory = opendir(path);
-  if (directory != NULL) {
-    while ((content = readdir(directory))) {
+
+  if (directory == NULL) {
+    closedir(directory);
+    return;
+  }
+
+  while ((content = readdir(directory))) {
       flagFolders = 0;
       if ((strcmp(content->d_name, ".") != 0) &&
           (strcmp(content->d_name, "..") != 0)) {
-        name2 = content->d_name;
-        path2 = malloc(strlen(path) + strlen(name2) + 2);
-        strcpy(path2, path);
-        if (!equalsRegex("/$", path2))
-          strcat(path2, "/");
-        strcat(path2, name2);
-        stat(path2, &fileStat);
+        nameFileContent = content->d_name;
+        pathBuilder = malloc(strlen(path) + strlen(nameFileContent) + 2);
+        strcpy(pathBuilder, path);
+        if (!equalsRegex("/$", pathBuilder))
+          strcat(pathBuilder, "/");
+        strcat(pathBuilder, nameFileContent);
+        stat(pathBuilder, &fileStat);
         mode_t mode = fileStat.st_mode;
-        if (strstr(name2, pattern)) {
+        if (strstr(nameFileContent, pattern)) {
           status = 1;
           amountFiles++;
           ++flagFolders;
-          isDirectory(mode) ? printf("%s%s%s\n", BLUECOLOUR, path2, ENDCOLOUR)
-                            : printf("%s%s%s\n", GREENCOLOUR, path2, ENDCOLOUR);
+          isDirectory(mode) ? printf("%s%s%s\n", BLUECOLOUR, pathBuilder, ENDCOLOUR)
+                            : printf("%s%s%s\n", GREENCOLOUR, pathBuilder, ENDCOLOUR);
         }
         if (isDirectory(mode)) {
           if (++flagFolders == 2) {
             amountFolders++;
             amountFiles--;
           }
-          findFile(path2, pattern);
+          findFile(pathBuilder, pattern);
         }
       }
-    }
   }
   closedir(directory);
 }
